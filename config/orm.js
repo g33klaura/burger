c3945911771ce0805120fcfe35bf62b0e016873d
Laura Boetcher <g33klaura@gmail.com>
2017-11-05 14:58:37 -0500

    Initial connection and orm requirements

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+// Import MySQL connection.
+var connection = require("./connection.js");
+
+/* Need method functions for these:
+selectAll()
+insertOne()
+updateOne()
+*/
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
