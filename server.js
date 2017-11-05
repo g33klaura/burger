79df431168c62e344e3830024c39e3959b547483
Laura Boetcher <g33klaura@gmail.com>
2017-11-05 18:10:06 -0500

    Add handlebars and express setup

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 // Starting point script
 
-// REQUIREMENTS ====================
-// =================================
-
+// REQUIREMENTS ========================================
+//
 const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
 // Might be needed in a diff script...
-const mysql = require('mysql');
-const expressHandlebars = require('express-handlebars');
+// const mysql = require('mysql');
+const exphbs = require('express-handlebars');
+
+let PORT = process.env.PORT || 3000;
+
+
+// SETUP ===============================================
+//
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// Will this point to 'test.html'?
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// ROUTING =============================================
+//
+// Import routes and give the server access to them.
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+// app.listen(PORT);
+app.listen(PORT, function() {
+  console.log('App listening on PORT: ' + PORT);
+});
