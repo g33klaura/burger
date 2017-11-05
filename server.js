226ab5f1af616de8f0db8aeb531ecfbcfacd2def
Laura Boetcher <g33klaura@gmail.com>
2017-11-04 22:04:47 -0400

    Initialize dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+// Starting point script
+
+// REQUIREMENTS ====================
+// =================================
+
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+// Might be needed in a diff script...
+const mysql = require('mysql');
+const expressHandlebars = require('express-handlebars');
