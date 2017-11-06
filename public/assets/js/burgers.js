d3eda7f832e76617767774c544788d3e3ba38482
Laura Boetcher <g33klaura@gmail.com>
2017-11-05 23:04:51 -0500

    Add click function for add-burger button

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 
 $(function() {
 
-	// Need "devour" buttons in a partial***
+	// Need 'devour' buttons in a partial***
 	// checkout cat-block in partials
 	$('.change-eat').on('click', function(event) {
 		let id = $(this).data('id');
@@ -25,4 +25,27 @@ $(function() {
     );
 	});
 
+	// Click function for '#add-burger' button
+	$('#add-burger').on('submit', function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    let newBurger = {
+      burger_name: $('#new-burger').val().trim(),
+      // devoured: $('[name=sleepy]:checked').val().trim()
+      // My app doesn't have a sleepy/not sleepy radio button...
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new delicious burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 });  //Closes document-ready
